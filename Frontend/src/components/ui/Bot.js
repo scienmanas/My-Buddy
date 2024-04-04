0b7feeae77a1a557822e5a8508fb433b76d6cfc0
Manas <iamscientistmanas@gmail.com>
2024-04-04 11:58:28 +0530

    Bot chat design complete

--- Frontend/src/components/ui/Bot.js
+++ Frontend/src/components/ui/Bot.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import MarkDownConverter from './MarkDownConverter'
-import botLogo from '../../assets/logo/logo.png'
+import botLogo from '../../assets/logo/logo_transparent_48px.png'
+import '../../styles/bot.css'
 
 export default function Bot(props) {
     return (
-        <div className='bot flex flex-row text-sm sm:text-base lg:text-lg w-[72%] '>
-            <div className="bot-logo w-4">
+        <div className='bot flex flex-row text-sm sm:text-base lg:text-lg w-[72%] items-start gap-[5px]'>
+            <div className="bot-logo w-8 flex justify-center items-center">
                 <img
                     src={botLogo}
                     alt=""
+                    className='w-8 h-8 rounded-full bg-white p-1'
                 />
             </div>
-            <div className="text">
-                <MarkDownConverter chat={props.text} />
+            <div className="text-bot-box flex-1 flex items-center">
+                <div className="text-bot-box-content px-2 py-1 bg-gradient-to-br from-[#6955dcdf] to-[#3d153b] w-fit">
+                    <MarkDownConverter chat={props.text} />
+                </div>
             </div>
         </div>
     )
--- Frontend/src/components/ui/User.js
+++ Frontend/src/components/ui/User.js
@@ -1,19 +1,20 @@
 import React from 'react'
-import MarkDownConverter from './MarkDownConverter'
-import userLogo from '../../assets/users/man.png'
+import MarkDownConverter from './MarkDownConverter';
+import userLogo from '../../assets/users/man.png';
+import '../../styles/user.css';
 
 export default function User(props) {
     return (
-        <div className='user flex flex-row text-sm sm:text-base lg:text-lg items-center place-content-end'>
-            <div className="bot-logo w-4">
-                <img
-                    src={userLogo}
-                    alt=""
-                />
-            </div>
-            <div className="text w-fit">
-                <MarkDownConverter chat={props.text} />
+        <div className="user w-full flex justify-end">
+            <div className='user-box flex flex-row-reverse items-center max-w-[75%] '>
+                <div className="bot-logo w-4">
+                    <img src={userLogo} alt="" />
+                </div>
+                <div className="text-user-box px-2 py-1 bg-transparent bg-gradient-to-tr to-[#C33764] from-[#1D2671] text-sm sm:text-base lg:text-lg">
+                    <MarkDownConverter chat={props.text} />
+                </div>
             </div>
         </div>
+
     )
 }
