bc9e449fdcf9f32c6e8d6df48726b3699a02131a
Manas <iamscientistmanas@gmail.com>
2024-04-13 13:39:54 +0530

    Add Feature-landing

--- Frontend/src/components/landing/Examples.js
+++ Frontend/src/components/landing/Examples.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Examples() {
   return (
-    <div>Examples</div>
+    <div className='example-tab flex flex-col gap-5 w-full h-fit'>
+      <div className="head-text-example">
+
+      </div>
+      <div className="feautures-display-example">
+
+      </div>
+    </div>
   )
 }
