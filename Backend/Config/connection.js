f72a8dc7813f396e3ce2f5e4c51015df982035a0
Manas <iamscientistmanas@gmail.com>
2024-04-26 19:43:00 +0530

    remove clg

--- Backend/Config/connection.js
+++ Backend/Config/connection.js
@@ -2,12 +2,11 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect(process.env.MONGODB_URL); 
+		await mongoose.connect(process.env.MONGODB_URL);
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
 	}
-	console.log(process.env.MONGODB_URL)
 };
 
 export default connectToMongoDB;
