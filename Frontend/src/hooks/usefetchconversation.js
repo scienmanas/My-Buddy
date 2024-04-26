27769737d9a62e19294f1b2900caf69b4e5c01d1
Manas <iamscientistmanas@gmail.com>
2024-04-26 14:47:57 +0530

    Fix frontend

--- Frontend/src/hooks/usefetchconversation.js
+++ Frontend/src/hooks/usefetchconversation.js
@@ -7,7 +7,7 @@ export const useFetchConversation = () => {
     const { authUser, mode, selectedchat } = useGlobalContext();
 
     const fetchconversation = async () => {
-        const res = await fetch(`https://my-buddy.onrender.com/api/getconversation/${selectedchat}`, {
+        const res = await fetch(`https://my-buddy.onrender.com/message/api/getconversation/${selectedchat}`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
